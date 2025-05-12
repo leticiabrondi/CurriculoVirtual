4f332c19e9b7151d0d51ab5afc0b0727206a7d5c
Letícia Brondi <leebrondi@gmail.com>
2025-05-12 11:38:22 -0300

    atualizando projetos

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -8,28 +8,28 @@ document.addEventListener("DOMContentLoaded", () => {
   
     const projetos = {
       projeto1: {
-        title: "Projeto 1",
-        img: "../assets/img/projetos/projeto.png",
+        title: "Suave Mente",
+        img: "../assets/img/projetos/suavemente.png",
         desc: "Criei esse projeto pra explorar mais minha criatividade e treinar o uso de [ferramenta/linguagem]. A ideia era fazer algo que fosse bonito, funcional e que tivesse a ver com o que tô aprendendo. Usei [tecnologias] e foquei bastante em [interface, responsividade, organização, etc.]. Foi um exercício massa de [ex: colocar em prática tudo que venho estudando] e me fez perceber o quanto evoluí em [área X].",
         link: "https://github.com/leticiabrondi"
       },
       projeto2: {
-        title: "Projeto 2",
-        img: "../assets/img/projetos/projeto.png",
+        title: "Cardfólio",
+        img: "../assets/img/projetos/cardfolio.png",
         desc: "Criei esse projeto pra explorar mais minha criatividade e treinar o uso de [ferramenta/linguagem]. A ideia era fazer algo que fosse bonito, funcional e que tivesse a ver com o que tô aprendendo. Usei [tecnologias] e foquei bastante em [interface, responsividade, organização, etc.]. Foi um exercício massa de [ex: colocar em prática tudo que venho estudando] e me fez perceber o quanto evoluí em [área X].",
         link: "https://github.com/leticiabrondi"
       },
       projeto3: {
-        title: "Projeto 3",
-        img: "../assets/img/projetos/projeto.png",
+        title: "READ4YOU.md",
+        img: "../assets/img/projetos/read4you.png",
         desc: "Criei esse projeto pra explorar mais minha criatividade e treinar o uso de [ferramenta/linguagem]. A ideia era fazer algo que fosse bonito, funcional e que tivesse a ver com o que tô aprendendo. Usei [tecnologias] e foquei bastante em [interface, responsividade, organização, etc.]. Foi um exercício massa de [ex: colocar em prática tudo que venho estudando] e me fez perceber o quanto evoluí em [área X].",
         link: "https://github.com/leticiabrondi"
       },
       projeto4: {
-        title: "Projeto 4",
-        img: "../assets/img/projetos/projeto.png",
-        desc: "Criei esse projeto pra explorar mais minha criatividade e treinar o uso de [ferramenta/linguagem]. A ideia era fazer algo que fosse bonito, funcional e que tivesse a ver com o que tô aprendendo. Usei [tecnologias] e foquei bastante em [interface, responsividade, organização, etc.]. Foi um exercício massa de [ex: colocar em prática tudo que venho estudando] e me fez perceber o quanto evoluí em [área X].",
-        link: "https://github.com/leticiabrondi"
+        title: "Book redesign",
+        img: "../assets/img/projetos/redesign.png",
+        desc: "Este projeto consiste no redesign da capa do livro O Dragão Vermelho, escrito por Thomas Harris. A tarefa foi realizada como parte das aulas de design do meu curso técnico de multimeios. O objetivo foi criar uma nova abordagem visual para a obra, mantendo a essência do suspense e mistério, mas ao mesmo tempo explorando uma estética mais moderna e impactante. A capa reformulada busca refletir o tom sombrio e psicológico da narrativa, enquanto oferece uma experiência visual que chama a atenção de novos leitores.",
+        link: "https://www.behance.net/gallery/218584917/BOOK-REDESIGN-O-dragao-vermelho"
       }
     };
   
